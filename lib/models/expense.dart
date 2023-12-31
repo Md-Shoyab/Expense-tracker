2db2e2b6165489006f05bc8941a267e51b5c8ae7
Md Shoyab <mdshoyab06@gmail.com>
2023-12-31 23:07:38 +0530

    Updated Expenses Model using Categories Enums

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,16 +1,20 @@
 import 'package:uuid/uuid.dart';
 
+enum Category { food, travel, work, leisure }
+
 const uuid = Uuid();
 
-class Expenses {
-  Expenses({
+class Expense {
+  Expense({
     required this.title,
     required this.amount,
     required this.date,
+    required this.category,
   }) : id = uuid.v4();
 
   final String id;
   final String title;
-  final String amount;
+  final double amount;
   final DateTime date;
+  final Category category;
 }
