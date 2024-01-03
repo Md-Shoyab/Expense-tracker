f05cb3e36fb037ac951e612d614014c8840c3c87
Md Shoyab <mdshoyab06@gmail.com>
2024-01-03 23:59:01 +0530

    Updated Expense Item and Added Expense String

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -10,7 +10,31 @@ class ExpenseItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Text(expense.title),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20.0,
+          vertical: 16.0,
+        ),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(height: 4.0),
+            Row(
+              children: [
+                Text('\$${expense.amount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    const Icon(Icons.alarm),
+                    const SizedBox(width: 8.0),
+                    Text(expense.date.toString())
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
