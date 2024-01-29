08bcfb0167031ec6bdc27e2e030638be81a4c72f
Md Shoyab <mdshoyab06@gmail.com>
2024-01-30 00:14:50 +0530

    ExpenseBucket.forCategory new Named Constructor created

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -40,12 +40,21 @@ class ExpenseBucket {
     required this.expenses,
   });
 
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
   final Category category;
   final List<Expense> expenses;
 
   double get totalExpenses {
     double sum = 0;
+    for (final expense in expenses) {
+      sum += expense.amount;
 
+      /// sum = sum + expense.amount
+    }
     return sum;
   }
 }
