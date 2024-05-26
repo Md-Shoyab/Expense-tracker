4e51037c7c531af3d7da012e2de05f7b42ab31a8
Md Shoyab <mdshoyab06@gmail.com>
2024-05-26 17:55:31 +0530

    made response according to width size

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -68,6 +68,7 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     Widget mainContent = const Center(
       child: Text('No Expenses Found. Start Adding Some:)'),
     );
@@ -89,12 +90,19 @@ class _ExpensesState extends State<Expenses> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Chart(expenses: _registeredExpenses),
-          Expanded(child: mainContent),
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                Chart(expenses: _registeredExpenses),
+                Expanded(child: mainContent),
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(child: Chart(expenses: _registeredExpenses)),
+                Expanded(child: mainContent),
+              ],
+            ),
     );
   }
 }
